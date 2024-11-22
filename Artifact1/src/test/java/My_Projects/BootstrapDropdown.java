9a11353a70dd5c87afa418988374225491a2aaf3
Vinoda123 <14vinodakn@gmail.com>
2024-11-22 12:05:22 +0530

    push Commit

--- Artifact1/src/test/java/My_Projects/BootstrapDropdown.java
+++ Artifact1/src/test/java/My_Projects/BootstrapDropdown.java
@@ -12,25 +12,17 @@ public class BootstrapDropdown {
 	
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Vinoda\\Selenium Webdriver\\Gecko Driver\\geckodriver-v0.35.0-win64\\geckodriver.exe");
 		FirefoxDriver driver = new FirefoxDriver();
-		driver.get("https://www.automationtestinginsider.com/2019/12/bootstrap-dropdown-example_12.html");
+		driver.get("https://magento.softwaretestingboard.com/customer/account/login/");
 		driver.manage().window().maximize();
 
-		driver.findElement(By.xpath("//button[@id='bootstrapmenu']")).click();
-		List<WebElement> options =  driver.findElements(By.xpath("//ul[@class='dropdown-menu']//li/a"));
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("vinodakn.2024@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("Vinu.Niha@2024");
+		driver.findElement(By.xpath("//*[@id=\"send2\"]/span")).click();
 		
-		for (WebElement webElement : options) {
-			
-			String value = webElement.getText();
-			System.out.println(value);
-			 
-			if(value.equalsIgnoreCase("contact us")) {
-				webElement.click();
-				break;
+		driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[2]/span")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a")).click();
 			}
 			 
-		}
-		
 		 //Thread.sleep(2000); driver.close();class data-class-idn aria-hidden = 'false'
-	}
-
 }
